9de6277419ba9c6ee8f600d21391d6f9798be473
Z790M <diiioman@foxmail.com>
2023-05-17 20:14:37 +0800

    ###课堂代码,作业

--- Work/src/com/qian/school_2023/May/exercise05_17/work/p169/PropertyRead.java
+++ Work/src/com/qian/school_2023/May/exercise05_17/work/p169/PropertyRead.java
@@ -1,4 +1,27 @@
 package com.qian.school_2023.May.exercise05_17.work.p169;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyRead {
+    public static void getProperty(){
+        Properties prop = new Properties();
+        try{
+            FileInputStream in = new FileInputStream("C:\\Users\\Z790M\\Documents\\Files\\Java\\JavaWorkCode\\Work\\src\\com\\qian\\school_2023\\May\\exercise05_17\\work\\ch18\\drivers.properties");
+            prop.load(in);
+            String driver = prop.getProperty("drivers");
+            String url = prop.getProperty("url");
+            String userName = prop.getProperty("user");
+            String password = prop.getProperty("password");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args){
+        getProperty();
+    }
 }
