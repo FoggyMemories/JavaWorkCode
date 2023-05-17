cce207033ac3456a65cea148ae04df68f86958a0
FoggyMemories <diiioman@foxmail.com>
2023-05-17 16:36:00 +0800

    ###课堂代码,作业

--- Work/src/com/qian/school_2023/May/exercise05_17/demo/ListDemo.java
+++ Work/src/com/qian/school_2023/May/exercise05_17/demo/ListDemo.java
@@ -1,6 +1,7 @@
 package com.qian.school_2023.May.exercise05_17.demo;
 
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Vector;
 
 public class ListDemo {
@@ -14,6 +15,14 @@ public class ListDemo {
         for (int i = 0; i < 3; i++) {
             list.add(i * 10);
         }
+
+        ListIterator lit = list.listIterator();
+
+        while (lit.hasNext()){
+            System.out.print(lit.next() + " ");
+        }
+
+        System.out.println();
 
         System.out.println(list);
     }
